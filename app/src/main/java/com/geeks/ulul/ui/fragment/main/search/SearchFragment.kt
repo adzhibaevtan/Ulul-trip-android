d7ade8ad89d96af65e42616b16d6c9149c3722f6
Aktan <86038270+jstAga@users.noreply.github.com>
2023-03-11 20:11:33 +0600

    ADD filter category

--- app/src/main/java/com/geeks/ulul/ui/fragment/main/search/SearchFragment.kt
+++ app/src/main/java/com/geeks/ulul/ui/fragment/main/search/SearchFragment.kt
@@ -1,8 +1,10 @@
 package com.geeks.ulul.ui.fragment.main.search
 
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geeks.ulul.core.base.BaseFragment
+import com.geeks.ulul.data.model.FilterModel
 import com.geeks.ulul_trip_android.R
 
 import com.geeks.ulul_trip_android.databinding.FragmentSearchBinding
@@ -14,4 +16,16 @@ class SearchFragment :
     BaseFragment<FragmentSearchBinding, SearchViewModel>(R.layout.fragment_search) {
     override val binding: FragmentSearchBinding by viewBinding(FragmentSearchBinding::bind)
     override val viewModel by viewModels<SearchViewModel>()
+    private val filter by lazy { FilterModel() }
+
+    override fun initListeners() {
+        super.initListeners()
+        setCategory()
+    }
+
+    private fun setCategory() {
+        binding.etCategory.doAfterTextChanged {
+            filter.category = it.toString()
+        }
+    }
 }
